6965697ee50eac47c74a7c31f8a755311d95b977
reihori <151887312+reihori@users.noreply.github.com>
2024-10-10 20:27:48 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+if ! grep -q Ubuntu /etc/os-release; then
+	echo 'This script is only compatible with Ubuntu.'
+	exit 1
+fi
+
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-ln -s $script_dir/zshrc $HOME/.zshrc
-mkdir -p $HOME/.config && ln -s $script_dir/starship.toml $HOME/.config
+ln -si $script_dir/zshrc $HOME/.zshrc
+mkdir -p $HOME/.config && ln -si $script_dir/starship.toml $HOME/.config
