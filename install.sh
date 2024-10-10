975e2fa60574161639d62ab3c5bddaf6019aaaf6
reihori <151887312+reihori@users.noreply.github.com>
2024-10-10 22:09:48 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,22 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-ln -si $script_dir/zshrc $HOME/.zshrc
-mkdir -p $HOME/.config && ln -si $script_dir/starship.toml $HOME/.config
+echo_eval() {
+	echo "> $1"
+	eval $1
+}
+
+# zsh
+echo_eval "ln -fs $script_dir/zshrc $HOME/.zshrc"
+dotzshdir="$HOME/.zsh"
+plugindir="$dotzshdir/plugins"
+echo_eval "rm -rf $plugindir"
+echo_eval "git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions $plugindir/zsh-autosuggestions 2> /dev/null"
+echo_eval "git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting $plugindir/zsh-syntax-highlighting 2> /dev/null"
+echo_eval "git clone --depth 1 https://github.com/olets/zsh-abbr $plugindir/zsh-abbr 2> /dev/null"
+
+# vim
+echo_eval "ln -fs $script_dir/vimrc $HOME/.vimrc"
+
+# starship
+echo_eval "mkdir -p $HOME/.config && ln -fs $script_dir/starship.toml $HOME/.config"
