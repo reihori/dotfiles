35ec7544b20a7accd7ea30ecbce4d8d3b1647112
reihori <151887312+reihori@users.noreply.github.com>
2025-07-10 08:50:37 +0900

    Update .config/zsh/zshrc.d/50-completion.zsh

--- dot_config/zsh/zshrc.d/50-completion.zsh
+++ dot_config/zsh/zshrc.d/50-completion.zsh
@@ -2,8 +2,8 @@ fpath=("$XDG_DATA_HOME/zsh/site-functions" $fpath)
 typeset -U fpath FPATH
 
 if ! typeset -f _comp_init &> /dev/null; then
-  autoload -Uz compinit
-  compinit
+    autoload -Uz compinit
+    compinit
 fi
 
 zstyle ':completion:*' menu select
