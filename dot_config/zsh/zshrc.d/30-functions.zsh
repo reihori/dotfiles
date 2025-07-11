fb7669641e59743cdb2034c0b6f355dc098fa3c0
reihori <151887312+reihori@users.noreply.github.com>
2025-07-11 10:07:54 +0900

    Update .config/zsh/zshrc.d/30-functions.zsh

--- dot_config/zsh/zshrc.d/30-functions.zsh
+++ dot_config/zsh/zshrc.d/30-functions.zsh
@@ -3,10 +3,20 @@ function chpwd() {
 }
 
 function chezmoi-edit-apply() {
-    [[ -n "$(chezmoi status)" ]] && return
+    if ! command -v chezmoi &> /dev/null; then
+        echo 'chezmoi is not installed.'
+        return 1
+    fi
+    if [[ -n "$(chezmoi status)" ]]; then
+        echo 'Files differ from chezmoi.'
+        return 1
+    fi
     local file="$(chezmoi managed --include=files | fzf --reverse)"
     local filepath="$HOME/$file"
-    [[ ! -f $filepath ]] && return
-    vim $filepath
+    if [[ ! -f $filepath ]]; then
+        echo "$filepath does not exist."
+        return 1
+    fi
+    ${EDITOR:-vim} $filepath
     chezmoi add $filepath
 }
