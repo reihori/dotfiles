e58c87d6ad3b8dd51fd753e8f061158788624a3f
reihori <151887312+reihori@users.noreply.github.com>
2025-07-13 08:47:09 +0900

    Update .config/zsh/zshrc.d/50-completion.zsh

--- dot_config/zsh/zshrc.d/50-completion.zsh
+++ dot_config/zsh/zshrc.d/50-completion.zsh
@@ -1,11 +1,9 @@
+fpath=("$XDG_DATA_HOME/zsh/site-functions" $fpath)
+typeset -U fpath FPATH
+
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
-fpath=("$XDG_DATA_HOME/zsh/site-functions" $fpath)
-typeset -U fpath FPATH
-
-if ! typeset -f _comp_init &> /dev/null; then
-    autoload -Uz compinit
-    compinit
-fi
+autoload -Uz compinit
+compinit
