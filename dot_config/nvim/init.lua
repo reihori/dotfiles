5b994a1e12d9da45ae82cfaf4c20334a61535359
reihori <151887312+reihori@users.noreply.github.com>
2025-10-12 08:56:38 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -108,7 +108,7 @@ require("lazy").setup({
     {
       "kylechui/nvim-surround",
       version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-      -- event = "VeryLazy",
+      event = "VeryLazy",
       opts = {},
     },
     {
