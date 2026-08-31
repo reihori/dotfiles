1773a5839e5387cce54de5d8b1aa81082ffd9312
reihori <151887312+reihori@users.noreply.github.com>
2026-08-31 19:34:42 +0900

    wip

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -18,70 +18,45 @@ vim.keymap.set("i", "<C-e>", "<Esc>A")
 vim.keymap.set("i", "<C-b>", "<Left>")
 vim.keymap.set("i", "<C-f>", "<Right>")
 
+local indent_configs = {
+  fish = { size = 4, expandtab = true },
+  lua = { size = 2, expandtab = true },
+  sh = { size = 4, expandtab = true },
+  zsh = { size = 4, expandtab = true },
+}
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "lua", "sh", "zsh" },
+  pattern = vim.tbl_keys(indent_configs),
   callback = function(ev)
-    local opts = {
-      lua = { shiftwidth = 2, tabstop = 2, softtabstop = 2, expandtab = true },
-      sh = { shiftwidth = 4, tabstop = 4, softtabstop = 4, expandtab = true },
-      zsh = { shiftwidth = 4, tabstop = 4, softtabstop = 4, expandtab = true },
-    }
-    local o = opts[ev.match]
-    if o then
-      for k, v in pairs(o) do
-        vim.opt_local[k] = v
-      end
+    local c = indent_configs[ev.match]
+    if c then
+      vim.opt_local.shiftwidth = c.size
+      vim.opt_local.tabstop = c.size
+      vim.opt_local.softtabstop = c.size
+      vim.opt_local.expandtab = c.expandtab
     end
   end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {
-    "javascript",
-    "lua",
-    "python",
-  },
   callback = function()
-    vim.treesitter.start()
+    pcall(vim.treesitter.start)
   end,
 })
 
 vim.pack.add({
   "https://github.com/catppuccin/nvim",
-  "https://github.com/neovim/nvim-lspconfig",
-  "https://github.com/nvim-lualine/lualine.nvim",
   "https://github.com/nvim-mini/mini.nvim",
-  "https://github.com/nvim-tree/nvim-web-devicons",
   "https://github.com/smoka7/hop.nvim",
-  "https://github.com/stevearc/conform.nvim",
 })
 
 vim.cmd.colorscheme("catppuccin-mocha")
 
-require("lualine").setup()
+require("mini.pairs").setup()
+
+require("mini.statusline").setup()
 
 local hop = require("hop")
 hop.setup()
 vim.keymap.set("n", "<Leader><Space>", function()
   hop.hint_words()
 end)
-
-require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "ruff_format" },
-  },
-})
-
-vim.lsp.enable({
-  "lua_ls",
-  "pyright",
-})
-
-require("mini.pairs").setup()
-local MiniPick = require("mini.pick")
-MiniPick.setup()
-vim.keymap.set("n", "<Leader>f", function()
-  MiniPick.builtin.files()
-  -- MiniPick.builtin.files({ tool = 'git' })
-end, { desc = "mini.pick.files" })
